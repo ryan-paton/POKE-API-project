42efb9795b458bef421ab123781e497888a5532a
Ryan Paton <rpaton333@gmail.com>
2020-07-02 16:11:49 +1200

    Created requestPOKEAPI and parsePOKEAPI functions which request and parse respectively JSON from POKEAPI Changed index.html to show results

--- pokeAPI.js
+++ pokeAPI.js
@@ -7,4 +7,22 @@ Ryan Paton
 2020-07-01
 */
 
+function parsePOKEAPI(response) {
+	// Parses the JSON reponse from a HTTP GET from POKEAPI
+	var pokemon = JSON.parse(response);
+	var html = "<img src=" + pokemon.sprites.front_default + " alt=\"pokemon\"/>";
+	
+	document.getElementById("results").innerHTML = html;
+}
 
+function requestPOKEAPI() {
+	// Sends a HTTP GET request to the POKEAPI based in input in the search field
+	var request = new XMLHttpRequest();
+	var searchText = document.getElementById("searchText").value;
+	
+	// Get user input from search bar
+	
+	request.onload(parsePOKEAPI);
+	request.open("GET", "https://pokeapi.co/api/v2/pokemon/" + searchText + "/");
+	request.send();
+}
