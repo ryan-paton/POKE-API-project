98ae2c74982d86921ec0233ed2000d32e563f156
Ryan Paton <rpaton333@gmail.com>
2020-07-17 12:47:52 +1200

    Search now looks at a list of known pokemon names befoire sending a request, also saves reponses from POKEAPI to reduce requests

--- pokeAPI.js
+++ pokeAPI.js
@@ -12,25 +12,49 @@ Ryan Paton
 
 function parsePOKEAPI() {
 	// Parses the JSON reponse from a HTTP GET from POKEAPI
-	// TODO: Cache responses
 	
 	var pokemon = JSON.parse(this.responseText);
-	var html = "<img src=" + pokemon.sprites.front_default + " alt=\"pokemon\"/>";
 	
+	// Save the result to reduce requests to the POKEAPI
+	pokemonList[pokemon.name] = pokemon;
+	
+	// Add image of the pokemon to the webpage
+	// TODO: Display cards with more than just the picture of a pokemon
+	var html = "<img src=" + pokemon.sprites.front_default + " alt=\"pokemon\"/>";
 	document.getElementById("results").innerHTML = html;
 }
 
-function requestPOKEAPI() {
+function requestPOKEAPI(searchName) {
 	// Sends a HTTP GET request to the POKEAPI based in input in the search field
-	// TODO: validate input
 	
 	var request = new XMLHttpRequest();
+	
+	request.addEventListener("load", parsePOKEAPI);
+	request.open("GET", "https://pokeapi.co/api/v2/pokemon/" + searchName + "/");
+	request.send();
+}
+
+function pokeSearch() {
+	/*
+	Compares the search input to the list of known pokemon names and then requests from
+	POKEAPI if the pokemon name exists
+	*/
 	
 	// Get user input from search bar
 	var searchText = document.getElementById("searchText").value;
 	searchText = searchText.toLowerCase();
 	
-	request.addEventListener("load", parsePOKEAPI);
-	request.open("GET", "https://pokeapi.co/api/v2/pokemon/" + searchText + "/");
-	request.send();
+	if (searchText in pokemonList) {
+		if (pokemonList[searchText] == undefined) {
+			requestPOKEAPI(searchText);
+		}
+		else {
+			//retrieve from cache
+		}
+	}
+	else {
+		// Display notice that pokemon does not exist
+		// TODO: suggest similar searches
+	}
 }
+
