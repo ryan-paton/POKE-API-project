f19b13c96a232791d9ed90af37f1a1b667bdae28
Ryan Paton <rpaton333@gmail.com>
2020-07-17 14:58:23 +1200

    Bug fix not able to access pokemonList

--- pokeAPI.js
+++ pokeAPI.js
@@ -16,7 +16,7 @@ function parsePOKEAPI() {
 	var pokemon = JSON.parse(this.responseText);
 	
 	// Save the result to reduce requests to the POKEAPI
-	pokemonList[pokemon.name] = pokemon;
+	window.pokemonList[pokemon.name] = pokemon;
 	
 	// Add image of the pokemon to the webpage
 	// TODO: Display cards with more than just the picture of a pokemon
@@ -44,8 +44,8 @@ function pokeSearch() {
 	var searchText = document.getElementById("searchText").value;
 	searchText = searchText.toLowerCase();
 	
-	if (searchText in pokemonList) {
-		if (pokemonList[searchText] == undefined) {
+	if (searchText in window.pokemonList) {
+		if (window.pokemonList[searchText] == undefined) {
 			requestPOKEAPI(searchText);
 		}
 		else {
